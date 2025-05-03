c92a72dd54675bd4a9746d6fb5a4ce862c90f1f0
Toktobekova-Nora-dev <nuraiymtoktobekkyzy507@gmail.com>
2025-05-03 16:42:10 +0600

    s

--- src/components/pages/Home/FirestSession.tsx
+++ src/components/pages/Home/FirestSession.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 import scss from "./FirestSession.module.scss";
+import photo from "../../../../public/img/photoHeader.jpeg";
+import Image from "next/image";
 
 const FirestSession = () => {
   return (
-    <div className={scss.FirestSession}>
+    <div className={scss.firestSession}>
       <div className="container">
-        <div className={scss.FirestSession}>FirestSession</div>
+        <div className={scss.firest}>
+          <div className={scss.firstLeft}>
+            <h1>
+              Latest Tech at Your
+              <br /> Fingertips
+            </h1>
+            <p>
+              Discover cutting-edge electronics and gadgets for
+              <br /> every need. From smartphones to laptops, we've got
+              <br /> you covered.
+            </p>
+            <div className={scss.child}>
+              <button className={scss.firetChild}>Shop Now</button>
+              <button className={scss.secondChild}>View Sales</button>
+            </div>
+          </div>
+          <div>
+            <Image src={photo} alt="img" />
+          </div>
+        </div>
       </div>
     </div>
   );
